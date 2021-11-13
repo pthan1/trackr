7721553eecd2d403d147a130cd23e50f6b9b5c25
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-11-12 19:51:38 -0700

    Add header

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -7,16 +7,14 @@ import { sidebarData } from './sidebarData';
 function Sidebar() {
   const sidebarLinks = sidebarData.map((item, key) => {
     return (
-      <NavLink to={item.link}
-      className="sidebar-navlink">
-        <li key={key} className="row'>
+      <NavLink to={item.link} className="sidebar-navlink">
+        <li key={key} className="row">
           <div id="icon">{item.icon}</div>
           <div id="title">{item.title}</div>
         </li>
       </NavLink>
     )
   })
-
 
 
   return (
